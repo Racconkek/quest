8cbeb23c868a1c37415158dfc3e7045525984cae
Racconkek <Lyuda-mirosh@mail.ru>
2020-10-28 11:48:54 +0500

    init

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function change_src() {
+    if (document.getElementById('photo').src.includes('cookie.jpg')) {
+        document.getElementById('photo').src = 'sdnem.png'
+    } else {
+        document.getElementById('photo').src = 'cookie.jpg'
+    }
+}
+
+window.onload = function () {
+    setInterval(change_src, 10000);
+    document.cookie = 'key=stegozavr'
+};
